65b5c66c1551ec946d9043bf74c597f30cbf433e
SandradelMarSoto <sandradelmarsoto@ciencias.unam.mx>
2019-03-31 17:50:48 -0600

    Practica terminada

--- mips/ejercicio01.asm
+++ mips/ejercicio01.asm
@@ -3,13 +3,4 @@
 # registro origen: $v1
 # registro destino: $v0
 
-# variables, aqui damos nuestros dos numeros
-	.data
-x:	.word 42 # contenido del registro de origen
-
-# código
-	.text
-# cargando registro v1
-	lw $v1 x
-# copiando contenido a v0
 	add $v0, $v1, $zero
